6cb654c50e58d889b8b6e5638a21327ad752b6c6
Luís Paulo Aganete Reis <luispaulo.aganete@hotmail.com>
2020-05-25 10:58:21 -0300

    Limpando projeto

--- routes.js
+++ routes.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const routes = express.Router()
 const  instructors =  require('./controllers/instructors')
-const  members =  require('./controllers/members')
-
 
 routes.get('/', function(req, res) {
     return res.redirect("/instructors")
@@ -31,26 +29,4 @@ routes.put("/instructors", instructors.put )
 //deletar
 routes.delete("/instructors", instructors.delete)
 
-
-//-----------------------------------------------------------------------------------------
-//== MEMBERS ==
-
-routes.get('/members', members.index)
-
-//Rota para CRIAR 
-routes.get('/members/create', members.create)
-
-//Criando rota (show)
-routes.get('/members/:id', members.show)
-
-//Rota para EDITAR 
-routes.get('/members/:id/edit', members.edit)
-
-//Configurando rota para trazer os dados do formulário para o backend
-routes.post("/members", members.post) 
-
-routes.put("/members", members.put )
-
-//deletar
-routes.delete("/members", members.delete)
 
